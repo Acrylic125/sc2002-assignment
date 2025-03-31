dc86578bfb92cd58496f3da018498fb2f53ca846
zhong liang <ztan127@e.ntu.edu.sg>
2025-03-31 23:38:22 +0800

    Update User.java
    added java docs

--- src/com/group6/users/User.java
+++ src/com/group6/users/User.java
@@ -1,3 +1,11 @@
+package com.group6.users;
+
+/**
+ * See {@link Applicant}.
+ * See {@link HDBOfficer}.
+ * See {@link HDBManager}.
+ */
+
 public abstract class User {
     private String name;
     private String nric;
@@ -5,6 +13,15 @@ public abstract class User {
     private String maritalStatus;
     private String password;
 
+    /**
+     * Constructor for User.
+     *
+     * @param name       name.
+     * @param nric     nric.
+     * @param age        age.
+     * @param maritalStatus maritalStatus.
+     * @param password password.
+     */
     public User(String name, String nric, int age, String maritalStatus, String password){
         this.name = name;
         this.nric = nric;
@@ -13,16 +30,58 @@ public abstract class User {
         this.password = password;
     }
 
+    /**
+     * UserRole getter
+     */
     public abstract UserRole getRole();
 
+    /**
+     * Name getter
+     *
+     * @return {@link #name}
+     */
     public String getName(){return name;}
+
+    /**
+     * Nric getter.
+     *
+     * @return {@link #nric}
+     */
     public String getNric(){return nric;}
+
+    /**
+     * Age getter.
+     *
+     * @return age
+     */
     public int getAge(){return age;}
+
+    /**
+     * Marital status getter.
+     *
+     * @return {@link #martialStatus}
+     */
     public String getMaritalStatus(){return maritalStatus;}
+
+    /**
+     * Password getter.
+     *
+     * @return {@link #password}
+     */
     public String getPassword(){return password;}
 
+    /**
+     * Password setter.
+     *
+     * @param password newPassword.
+     */
     public void setPassword(String newPassword){this.password = newPassword;}
 
+    /**
+    * Save users to file.
+    * 
+    * @return {@link #name, nric, age, maritalStatus, password}
+    */
     public String tofileString(){
         return name + ", " + nric + ", " + age + ", " + maritalStatus + ", " + password;
     }

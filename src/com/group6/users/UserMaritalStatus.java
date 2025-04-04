76c0a46764f5ba71eb43b6f2f5e32a25ac3d9c9b
zhong liang <ztan127@e.ntu.edu.sg>
2025-04-04 21:34:40 +0800

    Update UserMaritalStatus.java

--- src/com/group6/users/UserMaritalStatus.java
+++ src/com/group6/users/UserMaritalStatus.java
@@ -5,6 +5,24 @@ package com.group6.users;
  */
 public enum UserMaritalStatus {
 
-    MARRIED, SINGLE
+    MARRIED, SINGLE;
+
+    /**
+     * Converts a string to a UserMaritalStatus, case-insensitively.
+     *
+     * @param value The input string.
+     * @return The corresponding enum value, or null if invalid.
+     */
+    public static UserMaritalStatus fromString(String value) {
+        try {
+            return UserMaritalStatus.valueOf(value.trim().toUpperCase());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            return null;
+        }
+    }
+
+    public String toString() {
+        return name().charAt(0) + name().substring(1).toLowerCase();
+    }
 
 }
